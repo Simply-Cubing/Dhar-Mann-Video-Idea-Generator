ef18d5c19c7f380f1741691aeca39f201a3d05fb
Simply Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2022-11-02 14:12:55 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,9 +1,9 @@
 import random
-topics = ["School", "Cheating", "Bullying", "Pranking"]
+#word lists
 word1 = ["Kid", "Teacher", "Daughter", "Mean girls", "Rich mom", "Rich dad", "Adult", "Daughter", "Little sister", "Rich bride", "Spoiled brat", "Spoiled girl", "Twin", "Kid with down syndome", "Autistic kid" , "Little boy", "Karen"]
 word2 = ["backstabs", "pranks", "steals", "cheats", "cheats on", "breaks", "takes", "gets", "humiliates", "catches", "accuses", "shames",  "jealous of", "ditches", "bullies"]
-pt3 = ["on test", "test answers", "poor boyfriend", "fat kid", "school spelling bee",  ]
-pt4 = [", what happens will shock you", ", INSTANTLY regrets it", ", lives to regret it", ", what happens is shocking" ]
+pt3 = ["on test", "test answers", "poor boyfriend", "fat kid", "school spelling bee",]
+pt4 = [", what happens will shock you", ", INSTANTLY regrets it", ", lives to regret it", ", what happens is shocking"]
 seed0 = random.randint(0,3)
 seed1 = random.randint(0, 16)
 seed2 = random.randint(0, 14)
@@ -11,7 +11,6 @@ seed3 = random.randint(0,4)
 seed4 = random.randint(0, 3)
 
 #generates segments of the title
-t = topics[seed0]
 a = word1[seed1]
 b = word2[seed2]
 c = pt3[seed3]

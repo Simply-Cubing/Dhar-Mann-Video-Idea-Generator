0d5f6b4f4c4092acae79ca830413cafcf14b1082
Simply Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2023-10-29 17:37:44 +0800

    Update generator.py
    it now has a very goofy gui with bad code please teach me how to tkinter

--- generator.py
+++ generator.py
@@ -1,14 +1,30 @@
 import random
-#word lists
-word1 = ["Kid", "Teacher", "Daughter", "Mean girls", "Rich mom", "Rich dad", "Adult", "Daughter", "Little sister", "Rich bride", "Spoiled brat", "Spoiled girl", "Twin", "Kid with down syndome", "Autistic kid" , "Little boy", "Karen"]
-word2 = ["backstabs", "pranks", "steals", "cheats", "cheats on", "breaks", "takes", "gets", "humiliates", "catches", "accuses", "shames",  "jealous of", "ditches", "bullies"]
-pt3 = ["on test", "test answers", "poor boyfriend", "fat kid", "school spelling bee",]
-pt4 = [", what happens will shock you", ", INSTANTLY regrets it", ", lives to regret it", ", what happens is shocking"]
+from tkinter import *
+from PIL import ImageTk, Image  
+from topics import topics
+from word1 import word1
+from word2 import word2
+from pt3 import pt3
+from pt4 import pt4
+
+
+
+root = Tk()
+root.geometry("800x500")
+root.title("dhar mann video idea generator")
+
+generate_btn_img_original = PhotoImage(file="images/generate_button.png")
+generate_btn_img = generate_btn_img_original.subsample(3,3)
+
 seed0 = random.randint(0,3)
 seed1 = random.randint(0, 16)
 seed2 = random.randint(0, 14)
 seed3 = random.randint(0,4)
 seed4 = random.randint(0, 3)
+img_seed = random.randint(0, 10)
+
+dhar_img = ImageTk.PhotoImage(Image.open(f"images/dhar_mann_{img_seed}.jpeg"))
+
 
 #generates segments of the title
 a = word1[seed1]
@@ -16,4 +32,38 @@ b = word2[seed2]
 c = pt3[seed3]
 d = pt4[seed4]
 
-print(f"{a} {b} {c}{d}") 
+text = f"{a} {b} {c}{d}"
+
+label = Label(text=text, font=('arial',20))
+label.pack()
+label1 = Label(width = 400, height = 300, image=dhar_img)
+label1.pack()
+
+
+
+def generate():
+
+    seed0 = random.randint(0,3)
+    seed1 = random.randint(0, 16)
+    seed2 = random.randint(0, 14)
+    seed3 = random.randint(0,4)
+    seed4 = random.randint(0, 3)
+    img_seed = random.randint(0, 10)
+
+    global dhar_img 
+    dhar_img = ImageTk.PhotoImage(Image.open(f"images/dhar_mann_{img_seed}.jpeg"))
+
+
+    #generates segments of the title
+    a = word1[seed1]
+    b = word2[seed2]
+    c = pt3[seed3]
+    d = pt4[seed4]
+    
+    text = f"{a} {b} {c}{d}"
+    label.config(text=text)
+    label1.config(image=dhar_img)
+    return text
+
+generate_btn = Button(root,image= generate_btn_img,command=generate,width=500, height =200).pack(side=BOTTOM,padx=0,pady=0)
+root.mainloop()
